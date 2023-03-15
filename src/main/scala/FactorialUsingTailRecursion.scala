69b06920e048bfb0393121a16215168920c425c6
Shrasti0207 <shrasti.gupta@knoldus.com>
2023-03-15 11:26:20 +0530

    comment added

--- src/main/scala/FactorialUsingTailRecursion.scala
+++ src/main/scala/FactorialUsingTailRecursion.scala
@@ -1,10 +1,12 @@
 object FactorialUsingTailRecursion extends App{
-    def factorial(inputNumber: Int , accumulator: Int): Int = {
-      if(inputNumber < 0)
-        throw new IllegalArgumentException("factorial not defined for negative values")
-      if(inputNumber==0)
-          accumulator
-      else
-          factorial(inputNumber-1, inputNumber * accumulator)
+    // function that will return factorial
+    //this function will be executed recursively
+  def factorial(inputNumber: Int , accumulator: Int): Int = {
+    if(inputNumber < 0)
+      throw new IllegalArgumentException("factorial not defined for negative values")
+    else if(inputNumber==0)
+      accumulator
+    else
+      factorial(inputNumber-1, inputNumber * accumulator)
     }
 }
